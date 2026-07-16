6010c7abd0f9579d8b98e1aaef6e73afacb80318
deepblue21 <salihsrc42@gmail.com>
2026-07-16 19:44:29 +0300

    fix: bump Kotlin to 2.2.21 for LiteRT-LM metadata compatibility

--- nova-android/build.gradle.kts
+++ nova-android/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-}
+    // 2.2.21: litertlm-android 0.13.1, Kotlin 2.3 metadata'sıyla derlendi;
+    // Kotlin 2.2.x bunu okuyabilir ve kütüphanenin stdlib pini de 2
